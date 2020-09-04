75860389064d4caaf65f1376e8e3c86a5fd010a8
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-09-04 17:38:12 +0000

    create registraction component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/pages/authen/index.js';
+import Registration from './components/pages/registraction/index.js';
 
 function App() {
   return (
     <div className="App">
-         <Login/>
+         <Registration/>
     </div>
   );
 }
