8300807166a3ba2d39f0f1457292fb14186131b0
chetat <yekuwilfred@gmail.com>
2020-09-27 08:17:44 +0100

    Add protected routes and 404 page

--- src/Pages/Auth/Login/index.js
+++ src/Pages/Auth/Login/index.js
@@ -11,11 +11,11 @@ import {
 import './index.css';
 import validate from 'validate.js';
 import PropTypes from 'prop-types';
-import { useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import {
     useLocation
-  } from "react-router-dom";
-  
+} from "react-router-dom";
+
 import { loginUser } from '../../../Actions/authActions';
 
 const schema = {
@@ -87,80 +87,70 @@ const Login = () => {
         <div>
             <Grid container spacing={0} justify="center"
                 direction="row" >
-
-                <Grid item >
-                    <Grid container direction="column" justify="center"
-                        spacing={2}
-                        className="login-form" >
-
-                        <Paper variant="elevation" elevation={2} className="login-background" >
-                            <Grid item >
-                                <Typography component="h1" variant="h5" >
-                                    Sign in
+                <Paper variant="elevation" elevation={2} className="login-background" >
+                    <Grid item >
+                        <Typography component="h1" variant="h5" >
+                            Sign in
                             </Typography>
-                            </Grid>
-
-                            <Grid item >
-
-                                <form onSubmit={handleSignIn}>
-
-                                    <Grid container direction="column" spacing={2}>
-                                        <Grid item >
-                                            <TextField
-                                                error={hasError('email')}
-                                                fullWidth
-                                                helperText={
-                                                    hasError('email') ? formState.errors.email[0] : null
-                                                }
-                                                label="Email address"
-                                                name="email"
-                                                onChange={handleChange}
-                                                type="text"
-                                                value={formState.values.email || ''}
-                                                variant="outlined"
-                                            />
-
-                                        </Grid>
-                                        <Grid item >
-                                            <TextField
-                                                error={hasError('password')}
-                                                fullWidth
-                                                helperText={
-                                                    hasError('password') ? formState.errors.password[0] : null
-                                                }
-                                                label="Password"
-                                                name="password"
-                                                onChange={handleChange}
-                                                type="text"
-                                                value={formState.values.password || ''}
-                                                variant="outlined"
-                                            />
-
-                                        </Grid>
-                                        <Grid item >
-                                            <Button variant="contained"
-                                                    color="primary"
-                                                    type="submit"
-                                                    className="button-block">
-                                                Submit
-                                </Button>
-                                        </Grid>
-                                    </Grid>
-                                </form>
-
-                            </Grid>
-
-                            <Grid item >
-                                <Link href="#" variant="body2" >
-                                    Forgot Password ?
-                                </Link>
-                            </Grid>
-                        </Paper>
                     </Grid>
-                </Grid>
+
+                    <Grid item >
+
+                        <form onSubmit={handleSignIn}>
+
+                            <Grid container direction="column" spacing={2}>
+                                <Grid item >
+                                    <TextField
+                                        error={hasError('email')}
+                                        fullWidth
+                                        helperText={
+                                            hasError('email') ? formState.errors.email[0] : null
+                                        }
+                                        label="Email address"
+                                        name="email"
+                                        onChange={handleChange}
+                                        type="text"
+                                        value={formState.values.email || ''}
+                                        variant="outlined"
+                                    />
+
+                                </Grid>
+                                <Grid item >
+                                    <TextField
+                                        error={hasError('password')}
+                                        fullWidth
+                                        helperText={
+                                            hasError('password') ? formState.errors.password[0] : null
+                                        }
+                                        label="Password"
+                                        name="password"
+                                        onChange={handleChange}
+                                        type="text"
+                                        value={formState.values.password || ''}
+                                        variant="outlined"
+                                    />
+
+                                </Grid>
+                                <Grid item >
+                                    <Button variant="contained"
+                                        color="primary"
+                                        type="submit"
+                                        className="button-block">
+                                        Submit
+                                </Button>
+                                </Grid>
+                            </Grid>
+                        </form>
+
+                    </Grid>
+
+                    <Grid item >
+                        <Link href="#" variant="body2" >
+                            Forgot Password ?
+                                </Link>
+                    </Grid>
+                </Paper>
             </Grid>
-
-
         </div>
 
     );
--- src/Routes.js
+++ src/Routes.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Chat from './Pages/Chat';
 import Home from './Pages/Home';
+import UserProfile from './Pages/UserProfile';
 import Login from './Pages/Auth/Login';
 import Registration from './Pages/Auth/Registration'
-
+import NotFound from "./Pages/NotFound"
+import AuthRoute from './utils/AuthRoute';
 
 const Routes = () => {
     return(
@@ -12,7 +14,9 @@ const Routes = () => {
             <Route exact path="/" component={Home} />
             <Route exact path="/auth/register" component={Registration} />
             <Route exact path="/auth/login" component={Login} />
-            <Route exact path="/users/:userId/chat" component={Chat} />
+            <AuthRoute exact path="/users/:userId/chat" component={Chat} />
+            <AuthRoute exact path="/users/:id/profile" component={UserProfile} />
+            <Route path="*" component={NotFound}/>
         </Switch>
     );
 }
